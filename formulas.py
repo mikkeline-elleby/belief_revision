fcc71aa32b0c4e3c0201b945b7aa61281f326458
clarabjerrum <s194041@student.dtu.dk>
2024-04-21 15:25:11 +0200

    formulas syntax

--- formulas.py
+++ formulas.py
@@ -4,19 +4,19 @@ q = "q"
 r = "r"
 
 def conjunction(p, q):
-    return f"and({p}, {q})"
+    return f"and({p},{q})"
 
 def disjunction(p, q):
-    return f"or({p}, {q})"
+    return f"or({p},{q})"
 
 def negation(p):
     return f"not({p})"
 
 def implication(p, q):
-    return f"im({p}, {q})"
+    return f"imp({p},{q})"
 
 def equivalence(p, q):
-    return f"bi({p}, {q})"
+    return f"bi({p},{q})"
 
 # Example complex formulas
 formula1 = conjunction(p, q)  
